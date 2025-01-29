bec943893cab6266a8cda1163dcaed37b6c99bbd
tonni-akter-dev <tanniakhter123@gmail.com>
2025-01-30 00:06:45 +0600

    header

---
+++ src/components/CustomerFeedback.jsx
@@ -0,0 +1,9 @@
+const CustomerFeedback = () => {
+    return (
+        <div className='mt-[94px] max-w-[1400px] px-[120px] py-[25px] mx-auto'>
+            CustomerFeedback
+        </div>
+    )
+}
+
+export default CustomerFeedback
