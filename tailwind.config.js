7d9fa46092040c7510eb6ed9a0647da4fddc16f6
tonni-akter-dev <tanniakhter123@gmail.com>
2025-01-29 23:59:59 +0600

    add the desktop header design

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,10 +15,10 @@ export default {
             fontFamily: {
                 nunito: ["'Nunito Sans'", "sans-serif"],
             },
-            backgroundImage: {
-                gradientMain: "linear-gradient(90deg, #FFDF00 0%, #FDCC0D 100%)",
-                gradientMain2: "linear-gradient(90deg, #FDCC0D 0%, #FF9529 100%)",
-                gradientImg:
+            background: {
+                'gradient-main': "linear-gradient(90deg, #FFDF00 0%, #FDCC0D 100%)",
+                'gradient-main2': "linear-gradient(90deg, #FDCC0D 0%, #FF9529 100%)",
+                'gradientImg':
                     "linear-gradient(180deg, rgba(255, 223, 0, 0.10) 10%, rgba(242, 139, 15, 0.70) 70%, #FFF 100%), url(<path-to-image>) lightgray 50% / cover no-repeat",
             },
             boxShadow: {
