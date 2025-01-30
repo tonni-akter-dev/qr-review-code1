effeaa561d36817a4f0e64b1631ea09570c142d7
Arminara Jemi <105929289+armin-ara-jemi@users.noreply.github.com>
2025-01-30 18:18:21 +0600

    Add files via upload

---
+++ src/components/Work.jsx
@@ -0,0 +1,309 @@
+import React from "react";
+import gatherReviews from "../assets/gather reviews.png";
+import goodReview from "../assets/good review.png";
+import happyClient from "../assets/happy client.png";
+import issuse from "../assets/issuse.png";
+import reviewsBads from "../assets/reviews bads.png";
+import scan from "../assets/scan.png";
+import step01To2Svg from "../assets/step 01 to 2 svg.svg";
+import step3 from "../assets/step 3.svg";
+import stepArrowLast from "../assets/step arrow last.svg";
+import stepArrow from "../assets/step arrow.svg";
+import discount from "../assets/discount.png";
+import offer from "../assets/offer.png";
+
+
+
+
+export const Work = () => {
+   
+  return (
+    <div className='max-w-[1400px] px-[120px] py-[25px] mx-auto'>
+        <div className="bg-yellow-50 min-h-screen py-10 px-4 md:px-10">
+    <div className="flex flex-col w-[1201px] items-start gap-14 relative">
+      <div className="items-end flex flex-col justify-center gap-4 relative self-stretch w-full flex-[0_0_auto]">
+        <div className="items-center flex flex-col justify-center gap-4 relative self-stretch w-full flex-[0_0_auto]">
+          <p className="relative self-stretch mt-[-1.12px] font-title-46 font-[number:var(--title-46-font-weight)] text-black text-[length:var(--title-46-font-size)] text-center tracking-[var(--title-46-letter-spacing)] leading-[var(--title-46-line-height)] [font-style:var(--title-46-font-style)]">
+            Let’s know How it Work
+          </p>
+
+          <p className="relative w-[783px] font-body-18 font-[number:var(--body-18-font-weight)] text-body-text text-[length:var(--body-18-font-size)] text-center tracking-[var(--body-18-letter-spacing)] leading-[var(--body-18-line-height)] [font-style:var(--body-18-font-style)]">
+            Let’s explore how it works and discover the process step by step.
+            Understanding this will give you a clear view of how everything
+            comes together seamlessly.
+          </p>
+        </div>
+      </div>
+
+      <div className="relative w-[1200px] h-[2640px]">
+        <div className="absolute w-[1140px] h-[1027px] top-0 left-[30px]">
+          <div className="absolute w-[82px] h-20 top-0 left-[499px]">
+            <div className="relative w-20 h-20 rounded-[40px] [background:linear-gradient(180deg,rgb(253,204,13)_0%,rgb(255,149,41)_100%)]">
+              <div className="w-[22px] top-[26px] left-[29px] absolute [font-family:'Nunito_Sans-Bold',Helvetica] font-bold text-white text-4xl tracking-[-0.72px] leading-6 whitespace-nowrap">
+                1
+              </div>
+            </div>
+          </div>
+
+          <div className="inline-flex items-center gap-9 absolute top-[117px] left-0">
+            <img
+              className="relative w-[268px] h-[220px]"
+              alt="Scan"
+              src={scan}
+            />
+
+            <div className="w-[404px] h-[178px] items-center p-6 bg-[#ffffff] rounded-3xl shadow-[0px_6px_36px_#2823000d] flex gap-6 relative">
+              <div className="mt-[-4.00px] mb-[-4.00px] flex flex-col items-start gap-6 relative flex-1 grow">
+                <div className="relative self-stretch mt-[-1.00px] [font-family:'Nunito_Sans-Bold',Helvetica] font-bold text-black text-[26px] tracking-[-0.52px] leading-6">
+                  Scan &amp; Access Menu
+                </div>
+
+                <p className="relative self-stretch [font-family:'Nunito_Sans-Medium',Helvetica] font-medium text-body-text text-lg tracking-[-0.36px] leading-6">
+                  Place a branded QR code on the restaurant table for customers
+                  to scan, message on WhatsApp, and instantly receive the menu
+                  PDF.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="inline-flex items-center gap-9 absolute top-[467px] left-[434px]">
+            <div className="w-[404px] h-[178px] items-center p-6 bg-[#ffffff] rounded-3xl shadow-[0px_6px_36px_#2823000d] flex gap-6 relative">
+              <div className="mt-[-3.50px] mb-[-3.50px] flex flex-col items-start gap-6 relative flex-1 grow">
+                <div className="relative self-stretch mt-[-1.00px] [font-family:'Nunito_Sans-Bold',Helvetica] font-bold text-black text-2xl tracking-[-0.48px] leading-6">
+                  Gather Reviews &amp; Feedback
+                </div>
+
+                <p className="relative self-stretch [font-family:'Nunito_Sans-Regular',Helvetica] font-normal text-body-text text-lg tracking-[-0.36px] leading-6">
+                  Request user feedback, sending a Google review link for 4 or 5
+                  ratings, and gathering details for average reviews to save and
+                  address.
+                </p>
+              </div>
+            </div>
+
+            <img
+              className="relative w-[266px] h-[220px]"
+              alt="Gather reviews"
+              src={gatherReviews}
+            />
+          </div>
+
+          <img
+            className="absolute w-[620px] h-[722px] top-[38px] left-[274px]"
+            alt="Step to svg"
+            src={step01To2Svg}
+          />
+
+          <img
+            className="absolute w-[762px] h-[203px] top-[824px] left-[194px]"
+            alt="Step"
+            src={step3}
+          />
+
+          <div className="absolute w-[82px] h-20 top-[754px] left-[530px]">
+            <div className="relative w-[73px] h-[73px] top-[3px] left-[3px] bg-[url(/polygon-36.svg)] bg-[100%_100%]">
+              <div className="w-[23px] top-6 left-[25px] absolute [font-family:'Nunito_Sans-Bold',Helvetica] font-bold text-white text-4xl tracking-[-0.72px] leading-6 whitespace-nowrap">
+                3
+              </div>
+            </div>
+          </div>
+
+          <div className="absolute w-[82px] h-20 top-[537px] left-[235px]">
+            <div className="relative w-20 h-20 rounded-[40px] [background:linear-gradient(180deg,rgb(253,204,13)_0%,rgb(255,149,41)_100%)]">
+              <div className="w-[23px] top-[27px] left-7 absolute [font-family:'Nunito_Sans-Bold',Helvetica] font-bold text-white text-4xl tracking-[-0.72px] leading-6 whitespace-nowrap">
+                2
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col w-[433px] items-center gap-9 absolute top-[2206px] left-0">
+          <img
+            className="relative w-[338px] h-[220px]"
+            alt="Happy client"
+            src={happyClient}
+          />
+
+          <div className="h-[178px] items-center p-6 self-stretch w-full bg-[#ffffff] rounded-3xl shadow-[0px_6px_36px_#2823000d] flex gap-6 relative">
+            <div className="flex-col items-start flex-1 grow flex gap-6 relative">
+              <div className="relative self-stretch mt-[-1.00px] [font-family:'Nunito_Sans-Bold',Helvetica] font-bold text-black text-2xl text-center tracking-[-0.48px] leading-[30px]">
+                Happy Client
+              </div>
+
+              <p className="relative self-stretch [font-family:'Nunito_Sans-Regular',Helvetica] font-normal text-body-text text-lg text-center tracking-[-0.36px] leading-6">
+                Thank you so much for the discount! I really appreciate how
+                quickly you resolved my issue. I’ll definitely be visiting again
+                soon!
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col w-[425px] items-center gap-9 absolute top-[2206px] left-[775px]">
+          <img
+            className="relative w-[312px] h-[220px]"
+            alt="Group"
+            src={offer}
+          />
+
+          <div className="h-[178px] items-center p-6 self-stretch w-full bg-[#ffffff] rounded-3xl shadow-[0px_6px_36px_#2823000d] flex gap-6 relative">
+            <div className="flex-col items-start flex-1 grow flex gap-6 relative">
+              <div className="relative self-stretch mt-[-1.00px] [font-family:'Nunito_Sans-Bold',Helvetica] font-bold text-black text-2xl text-center tracking-[-0.48px] leading-[30px]">
+                Offer A Solution
+              </div>
+
+              <p className="relative self-stretch [font-family:'Nunito_Sans-Regular',Helvetica] font-normal text-body-text text-lg text-center tracking-[-0.36px] leading-6">
+                We’re happy to offer you a 10% discount, valid for 30 days! Show
+                it on your next visit, and we’ll verify it in our system. See
+                you soon!
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="absolute w-[415px] h-[577px] top-[1034px] left-0">
+          <div className="absolute w-9 h-[243px] top-[334px] left-[195px]">
+            <div className="relative h-[243px]">
+              <img
+                className="absolute w-1 h-[221px] top-0 left-4"
+                alt="Arrow"
+                src={stepArrow}
+              />
+
+             
+            </div>
+          </div>
+
+          <div className="flex flex-col w-[415px] items-center gap-9 absolute top-0 left-0">
+            <img
+              className="relative w-[330px] h-[220px]"
+              alt="Good review"
+              src={goodReview}
+            />
+
+            <div className="h-[178px] items-center p-6 self-stretch w-full bg-[#ffffff] rounded-3xl shadow-[0px_6px_36px_#2823000d] flex gap-6 relative">
+              <div className="flex-col items-start flex-1 grow flex gap-6 relative">
+                <div className="relative self-stretch mt-[-1.00px] [font-family:'Nunito_Sans-Bold',Helvetica] font-bold text-black text-2xl text-center tracking-[-0.48px] leading-6">
+                  Good Reviews &amp; Feedback
+                </div>
+
+                <p className="relative self-stretch [font-family:'Nunito_Sans-Regular',Helvetica] font-normal text-body-text text-lg text-center tracking-[-0.36px] leading-6">
+                  For 4⭐ or 5⭐ reviews, thank customers for their positive
+                  feedback.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="absolute w-[425px] h-[561px] top-[1627px] left-0">
+          <div className="absolute w-9 h-[243px] top-[318px] left-[195px]">
+            <div className="relative h-[243px]">
+              <img
+                className="absolute w-1 h-[221px] top-0 left-4"
+                alt="Arrow"
+                src={stepArrow}
+              />
+
+              <Vector className="!top-[207px] !absolute !w-9 !h-9 !left-0" />
+            </div>
+          </div>
+
+          <div className="flex flex-col w-[425px] items-center gap-9 absolute top-0 left-0">
+            <img
+              className="relative w-[310px] h-[220px]"
+              alt="Discount"
+              src={discount}
+            />
+
+            <div className="h-[178px] items-center p-6 self-stretch w-full bg-[#ffffff] rounded-3xl shadow-[0px_6px_36px_#2823000d] flex gap-6 relative">
+              <div className="flex-col items-start flex-1 grow flex gap-6 relative">
+                <div className="relative self-stretch mt-[-1.00px] [font-family:'Nunito_Sans-Bold',Helvetica] font-bold text-black text-2xl text-center tracking-[-0.48px] leading-[30px]">
+                  Discount
+                </div>
+
+                <p className="relative self-stretch [font-family:'Nunito_Sans-Regular',Helvetica] font-normal text-body-text text-lg text-center tracking-[-0.36px] leading-6">
+                  Send a 10% discount coupon, valid for 30 days, and notify the
+                  restaurant to verify it via CRM.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <img
+          className="absolute w-[331px] h-9 top-[2536px] left-[444px]"
+          alt="Step arrow last"
+          src={stepArrowLast}
+        />
+
+        <div className="absolute w-[404px] h-[576px] top-[1034px] left-[796px]">
+          <img
+            className="absolute w-9 h-[242px] top-[334px] left-[174px]"
+            alt="Step arrow"
+            src={stepArrow}
+          />
+
+          <div className="flex flex-col w-[404px] items-center gap-9 absolute top-0 left-0">
+            <img
+              className="relative w-[304.31px] h-[220px]"
+              alt="Reviews bads"
+              src={reviewsBads}
+            />
+
+            <div className="h-[178px] items-center p-6 self-stretch w-full bg-[#ffffff] rounded-3xl shadow-[0px_6px_36px_#2823000d] flex gap-6 relative">
+              <div className="flex-col items-start flex-1 grow flex gap-6 relative">
+                <div className="relative self-stretch mt-[-1.00px] [font-family:'Nunito_Sans-Bold',Helvetica] font-bold text-black text-2xl text-center tracking-[-0.48px] leading-8">
+                  Average/bad Reviews &amp; Feedback
+                </div>
+
+                <p className="relative self-stretch [font-family:'Nunito_Sans-Regular',Helvetica] font-normal text-body-text text-lg text-center tracking-[-0.36px] leading-6">
+                  Apologize and assure the customer their concerns are being
+                  addressed.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="absolute w-[425px] h-[560px] top-[1627px] left-[775px]">
+          <div className="absolute w-9 h-[242px] top-[318px] left-[195px]">
+            <div className="relative h-[242px]">
+              <img
+                className="absolute w-1 h-[221px] top-0 left-4"
+                alt="Arrow"
+                src={stepArrow}
+              />
+
+              <Vector className="!top-[206px] !absolute !w-9 !h-9 !left-0" />
+            </div>
+          </div>
+
+          <div className="flex flex-col w-[425px] items-center gap-9 absolute top-0 left-0">
+            <img
+              className="relative w-[340px] h-[220px]"
+              alt="Issuse"
+              src={issuse}
+            />
+
+            <div className="h-[178px] items-center p-6 self-stretch w-full bg-[#ffffff] rounded-3xl shadow-[0px_6px_36px_#2823000d] flex gap-6 relative">
+              <div className="flex-col items-start flex-1 grow flex gap-6 relative">
+                <div className="relative self-stretch mt-[-1.00px] [font-family:'Nunito_Sans-Bold',Helvetica] font-bold text-black text-2xl text-center tracking-[-0.48px] leading-[30px]">
+                  Acknowledge The Issue
+                </div>
+
+                <p className="relative self-stretch [font-family:'Nunito_Sans-Regular',Helvetica] font-normal text-body-text text-lg text-center tracking-[-0.36px] leading-6">
+                  Respond to the review with empathy. Let the customer know that
+                  you’ve read their feedback and you value their opinion.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+    </div>
+  );
+};
