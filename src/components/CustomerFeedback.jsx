af21c68dbe01cb246dececb29bea4a412fcf69d1
tonni-akter-dev <tanniakhter123@gmail.com>
2025-01-30 10:43:40 +0600

    update

--- src/components/CustomerFeedback.jsx
+++ src/components/CustomerFeedback.jsx
@@ -19,18 +19,23 @@ const CustomerFeedback = () => {
             <div>
                 <h2 className="text-primary  lg:text-[46px] text-2xl font-extrabold leading-normal text-center mb-4">Supercharge Your Customer Feedback Today!</h2>
                 <p className="text-[#A0A0A0] mb-[56px] lg:text-lg text-sm  text-center">Inspiring success through innovative solutions.</p>
-                <div className="grid lg:grid-cols-4 grid-cols-2 gap-6">
+                <div className="grid lg:grid-cols-4 grid-cols-2 gap-6 mb-9">
                     {superchargeData.map((item, index) => (
                         <div key={index} className="lg:p-6 p-4 rounded-[20px]" style={{ backgroundColor: item.bg_color }}>
                             <div className={`bg-${item.icon_bg}  mx-auto lg:p-5 p-[12px] w-fit rounded-full mb-6 flex justify-center items-center`} style={{ backgroundColor: item.icon_bg }}>
                                 <div >
-                                {item.icon(iconSize)}
+                                    {item.icon(iconSize)}
                                 </div>
                             </div>
                             <h3 className="text-[#222] lg:text-[22px] text-sm font-bold text-center mb-4 lg:whitespace-nowrap leading-[24px]">{item.title}</h3>
                             <p className="text-[#A0A0A0] lg:text-base text-xs text-center">{item.description}</p>
                         </div>
                     ))}
+                </div>
+
+                <div className="flex justify-center">  
+                    <button className="normal_btn py-3 px-9 rounded-full text-[#222] font-bold text-lg">Start Boosting Tour Reviews</button>
+
                 </div>
             </div>
         </div>
