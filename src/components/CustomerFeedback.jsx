80611c217fae47dffb344972996343daf8166ee0
tonni-akter-dev <tanniakhter123@gmail.com>
2025-01-30 10:40:13 +0600

    update

--- src/components/CustomerFeedback.jsx
+++ src/components/CustomerFeedback.jsx
@@ -1,6 +1,19 @@
+import { useEffect, useState } from "react";
 import { superchargeData } from "../utils/data"
 
 const CustomerFeedback = () => {
+
+    const [iconSize, setIconSize] = useState(window.innerWidth < 768 ? 27 : 40);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIconSize(window.innerWidth < 768 ? 27 : 40);
+        };
+
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
     return (
         <div className='lg:mt-[94px] mt-20 max-w-[1400px] lg:px-[120px] px-5 py-[25px] mx-auto'>
             <div>
@@ -9,8 +22,10 @@ const CustomerFeedback = () => {
                 <div className="grid lg:grid-cols-4 grid-cols-2 gap-6">
                     {superchargeData.map((item, index) => (
                         <div key={index} className="lg:p-6 p-4 rounded-[20px]" style={{ backgroundColor: item.bg_color }}>
-                            <div className={`bg-${item.icon_bg} mx-auto p-5 w-fit rounded-full mb-6`} style={{ backgroundColor: item.icon_bg }}>
-                                {item.icon()}
+                            <div className={`bg-${item.icon_bg}  mx-auto lg:p-5 p-[12px] w-fit rounded-full mb-6 flex justify-center items-center`} style={{ backgroundColor: item.icon_bg }}>
+                                <div >
+                                {item.icon(iconSize)}
+                                </div>
                             </div>
                             <h3 className="text-[#222] lg:text-[22px] text-sm font-bold text-center mb-4 lg:whitespace-nowrap leading-[24px]">{item.title}</h3>
                             <p className="text-[#A0A0A0] lg:text-base text-xs text-center">{item.description}</p>
